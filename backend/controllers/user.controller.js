e5007347bac90e38538626f31366ae492205d641
Gouri <gouriminocha@gmail.com>
2025-06-29 00:09:32 +0530

    Add logout functionality and integrate Redis for token management

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -3,7 +3,7 @@
 import userModel from '../models/user.model.js'
 import * as userService from '../services/user.service.js'
 import { validationResult } from 'express-validator'
-
+import redisClient from '../services/redis.service.js'
 
 export const createUserController = async (req, res) =>{
     //find errors throuhg express validator
@@ -76,4 +76,24 @@ export const profileController = async(req, res) =>{
     res.status(200).json({
         user: req.user
     })
+}
+
+export const logoutController = async(req,res) =>{
+    try{
+
+        const token = req.cookies?.token || req.headers?.authorization?.split(' ')[ 1];
+    
+        redisClient.set(token, 'logout','EX', 60*60*24);
+
+        res.status(200).json({
+            message: 'Logged out successfully'
+        });
+
+
+
+    }
+    catch(err){
+        console.log(err);
+        res.status(400).send(err.message);
+    }
 }
--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -1,6 +1,7 @@
 //it checks if user is logged in then it can access all other routes, otherwise unauthorized user
 
 import jwt from "jsonwebtoken";
+import redisClient from "../services/redis.service.js";
 
 
 export const authUser = async(req, res, next)=>{
@@ -10,6 +11,16 @@ export const authUser = async(req, res, next)=>{
         if(!token){
             return res.status(401).send({error: "Unauthorized User"});
         }
+
+    const isBlackListed = await redisClient.get(token);
+    //now user is logged out, so balcklisted token
+
+        if(isBlackListed){
+            res.cookie('token', '');
+
+            return res.status(401).send({error: 'Unauthorized User'})
+        }
+
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = decoded;
         next();
