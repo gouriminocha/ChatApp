488f14b6338f9cca34df85ef9acfb0ca9540aa7e
Gouri <gouriminocha@gmail.com>
2025-07-01 17:33:43 +0530

    Implement project creation functionality in Home component

--- _frontend/src/config/axios.js
+++ _frontend/src/config/axios.js
@@ -3,6 +3,9 @@ import axios from "axios";
 
 const axiosInstance = axios. create({
     baseURL: import.meta.env.VITE_API_URL,
+    headers:{
+        "Authorization":`Bearer ${localStorage.getItem('token')}` //token to be readed from local storaga and sent to header whenever a request to server made
+    }
 })
 
 export default axiosInstance;
