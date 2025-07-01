b55c3e34e9377d24a973eefb414832dd6759f3ee
Gouri <gouriminocha@gmail.com>
2025-07-01 23:53:44 +0530

    Add project screen and integrate project listing in Home component

--- _frontend/src/screens/Home.jsx
+++ _frontend/src/screens/Home.jsx
@@ -1,7 +1,8 @@
 import React, {useContext} from 'react'
 import {UserContext} from '../context/user.context'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import axios from '../config/axios.js'
+import { useNavigate } from 'react-router-dom'
 
 function Home() {
 
@@ -9,6 +10,8 @@ function Home() {
 
     const [isModalOpen, setIsModalOpen] = useState(false)
     const [projectName, setProjectName] = useState('')
+    const [project, setProject] = useState([])
+    const navigate = useNavigate();
 
     //to create a project by clicking on the icon
     function createProject(e){
@@ -26,16 +29,47 @@ function Home() {
       })
     }
 
+    useEffect(()=>{
+      axios.get('/projects/all').then((res)=>{
+       
+
+        setProject(res.data.projects)
+
+      }).catch(err=>{
+        console.log(err);
+      })
+    },[])
 
   return (
    <main className='p-4'>
-    <div className='projects'>
+    <div className='projects flex flex-wrap gap-3'>
       <button 
       onClick={() => setIsModalOpen(true)}
       className='project p-4 border border-slate-300 rounded-md'>
         New Project
         <i className="ri-link ml-2"></i>
       </button>
+
+      {
+        project.map((project)=>(
+          <div key ={project._id}
+          onClick={()=>{
+            navigate(`/project`,{
+              state: {project}
+            })
+          }}
+          className="project flex flex-col gap-2 cursor-pointer p-4 border border-slate-300 rounded-md min-w-52 hover:bg-slate-200">
+           <h2 className='font-semibold'>{project.name}</h2>
+            
+            <div className='flex gap-2'>
+            <p> <small> <i className='ri-user-line'></i>Collaborators:</small></p>
+              {project.users.length}
+            </div>
+            </div>
+        ))
+      }
+
+
       </div>
 
        {isModalOpen && (
