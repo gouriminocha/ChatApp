4d32b805a57b0ef01aff099883852665be7b1679
Gouri <gouriminocha@gmail.com>
2025-07-05 14:55:51 +0530

    Add file tree and code editor for managing project files

--- _frontend/src/screens/Project.jsx
+++ _frontend/src/screens/Project.jsx
@@ -37,7 +37,19 @@ const Project = () => {
   const {user} = useContext(UserContext)
   const messageBox = React.createRef()
   const [messages, setMessages] = useState([])
+  const [fileTree, setFileTree] = useState({
+    "app.js":{
+      content: `const express = require('express);`
+    },
+    "package.json":{
+      content: `{
+          "name: "temp-server,
+          }`
+    }
+  })
 
+  const [currentFile, setCurrentFile] = useState(null)
+  const [openFiles, setOpenFiles] = useState([])  //to open multiple files 
  const [users, setUsers] = useState([])
 
     const handleUserClick = (id) => {
@@ -241,6 +253,63 @@ const Project = () => {
 
                     </div>
                 </div>
+            </section>
+
+
+
+            <section className="right bg-red-50 flex-grow h-full flex">
+
+              <div className="explorer h-full max-w-64 min-w-52  bg-slate-200">
+                <div className="file-tree w-full">
+                 {
+                  Object.keys(fileTree).map((file, index)=>(
+                     <button onClick={()=> {setCurrentFile(file)
+                       setOpenFiles([...new Set([...openFiles, file])])
+                     }}
+                     className="tree-element p-2 cursor-pointer  px-4 flex items-center gap-2 bg-slate-300 w-full ">
+                    <p
+                    className='font-semibold text-lg'>
+                      {file}
+                    </p>
+                  </button>
+                  ))
+                 }
+                </div>
+              </div>
+              {currentFile && (
+              <div className="code-editor flex flex-col flex-grow h-full ">
+               <div className="top flex ">
+                {
+                  openFiles.map((file, index) =>(
+                    <button onClick={() => setCurrentFile(file)}
+                    className={`open-file cursor-pointer p-2 px-4 flex items-center w-fit gap-2 bg-slate-300 ${currentFile === file ? 'bg-slate-400' : ''}`}>
+                      <p className='font-semibold text-lg'>{file}</p>
+                    </button>
+                  ))
+                }
+               </div>
+               <div className="bottom flex flex-grow max-w-full shrink overflow-auto">
+                {
+                  fileTree[currentFile] &&(
+                    <textarea
+                    value={fileTree[currentFile].content}
+                    onChange={(e) =>{
+                      setFileTree({
+                        ...fileTree,
+                        [currentFile]:{
+                          content: e.target.value
+                        }
+                      })
+                    }}
+                    className='w-full h-full p-4 bg-slate-50 outline-none border-none'
+                  ></textarea>
+                  )
+                }
+               </div>
+
+              </div>
+              )}
+
             </section>
 
       {/* Modal for adding collaborators */}
